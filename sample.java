13162fb412f33c730553b23b52ec44459e2c5cdf
vigneshwaran0218 <89064632+vigneshwaran0218@users.noreply.github.com>
2021-08-23 03:21:18 -0400

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,6 +1,6 @@
 class Simple{
 public static void main(String args[]) throws InterruptedException{
 Thread.sleep(10000);
-System.out.println("Hello Java");
+System.out.println("Hello updated Java");
 }
 }
